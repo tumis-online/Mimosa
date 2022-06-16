7dabd442c3eaa47035569cb5a5ca2ddd1450012d
tim <t.dilger@uni-bielefeld.de>
2022-06-16 10:22:51 +0200

    Added GQL basic structure

---
+++ graphql_client.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from gql import Client, gql
+from gql.transport.aiohttp import AIOHTTPTransport
+
+
+async def main():
+    host = "localhost"
+    port = 13781
+    suffix = "/graphql"
+    api_endpoint = "http://" + host + ":" + str(port) + suffix
+    transport = AIOHTTPTransport(url=api_endpoint)
+
+    # Using `async with` on the client will start a connection on the transport
+    # and provide a `session` variable to execute queries on this connection
+    async with Client(
+        transport=transport,
+        fetch_schema_from_transport=True,
+    ) as session:
+
+        # Execute single query
+        query = gql(
+            """
+            query getContinents {
+              continents {
+                code
+                name
+              }
+            }
+        """
+        )
+
+        result = await session.execute(query)
+        print(result)
+
+
+asyncio.run(main())
