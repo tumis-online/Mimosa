8bdf9aab31b8626079fbdbbb5d978526613b523b
tim <t.dilger@uni-bielefeld.de>
2022-07-04 23:02:11 +0200

    Changed stt model structure

--- NeMo/asr/speech_recognition.py
+++ NeMo/asr/speech_recognition.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from os import path
 from os.path import exists
@@ -8,46 +9,45 @@ from nemo.collections.asr.models import asr_model
 
 sound_dir = "../sounds/"
 example_sound_dir = sound_dir + "examples/"
-
 pretrained_punctuation_model = "punctuation_en_distilbert"
 pretrained_asr_model = "stt_de_citrinet_1024"
-model: asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(pretrained_asr_model)
+nemo_asr_model: asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(pretrained_asr_model)
 
 
-async def stt_from_stream(stream):
-    pass
+# Handles Speech to Text from file or from stream
+class STTHandler:
 
+    def __init__(self, model):
+        self.model = model
 
-async def stt_from_file(model: asr_model, sample_file: path):
-    """
-    Takes sample file input and converts it into text.
-    :param model: pretrained NeMo asr model
-    :param sample_file: mono and sampled at 16Khz
-    :return:
-    """
-    if not exists(sample_file):
-        logging.error(f"Could not locate sample file {sample_file}. Exiting asr...")
-        exit(1)
-    logging.info(f"Found sample file {sample_file}. Continuing asr...")
-    transcriptions = model.transcribe([sample_file])
-    print(transcriptions)
+    async def stt_from_stream(self, stream):
+        pass
 
-    # this will also trigger model download
-    punctuation = nemo_nlp.models.PunctuationCapitalizationModel\
-        .from_pretrained(model_name=pretrained_punctuation_model)
-    res = punctuation.add_punctuation_capitalization(queries=transcriptions)
-    print(res)
+    async def stt_from_file(self, sample_file: path):
+        """
+        Takes sample file input and converts it into text.
+        :param sample_file: mono and sampled at 16Khz
+        :return:
+        """
+        if not exists(sample_file):
+            logging.error(f"Could not locate sample file {sample_file}. Exiting asr...")
+            exit(1)
+        logging.info(f"Found sample file {sample_file}. Continuing asr...")
+        transcriptions = self.model.transcribe([sample_file])
+        logging.debug(f"transcriptions: \n{transcriptions}")
 
-
-async def example_to_text(model: asr_model):
-    # This will initiate pre-trained model download from NGC
-    sample_file = f"{example_sound_dir}samples_thorsten-21.06-emotional_neutral.wav"
-    await stt_from_file(model, sample_file)
+        # this will also trigger model download
+        punctuation = nemo_nlp.models.PunctuationCapitalizationModel\
+            .from_pretrained(model_name=pretrained_punctuation_model)
+        res = punctuation.add_punctuation_capitalization(queries=transcriptions)
+        logging.info(res)
 
 
 async def main():
-    await example_to_text(model)
+    stt_handler = STTHandler(nemo_asr_model)
+    sample_file = f"{example_sound_dir}samples_thorsten-21.06-emotional_neutral.wav"
+    await stt_handler.stt_from_file(sample_file)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
