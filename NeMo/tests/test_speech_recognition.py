badab8355779f4827a2a402ab5ada84d3a26e2f4
tim <t.dilger@uni-bielefeld.de>
2022-08-27 08:49:05 +0200

    Adjusted asr test by similarity ratio

--- NeMo/tests/test_speech_recognition.py
+++ NeMo/tests/test_speech_recognition.py
@@ -1,9 +1,21 @@
 import pytest
+from difflib import SequenceMatcher
 
 from NeMo.src.asr.speech_recognition import EXAMPLE_SOUND_DIR, STTHandler
 
 EXAMPLE_SOUND_FILE: str = "samples_thorsten-21.06-emotional_neutral.wav"
-EXAMPLE_SOUND_FILE_TEXT: str = ""
+EXAMPLE_SOUND_FILE_TEXT: str = "Mißt wieder nichts geschafft."
+
+
+def check_similarity(a: str, b: str) -> bool:
+    """
+    Checks whether two strings are similar.
+    :param a: comparable string
+    :param b: comparable string
+    :return: true, if similarity ratio of strings is at least .9
+    """
+    similarity_threshold = 0.9
+    return SequenceMatcher(None, a, b).ratio() >= similarity_threshold
 
 
 class TestASR:
@@ -14,4 +26,4 @@ class TestASR:
         stt_handler = STTHandler()
         sample_file = f"{EXAMPLE_SOUND_DIR}{EXAMPLE_SOUND_FILE}"
         result = await stt_handler.stt_from_file(sample_file)
-        assert result == EXAMPLE_SOUND_FILE_TEXT
+        assert check_similarity(result, EXAMPLE_SOUND_FILE_TEXT)
